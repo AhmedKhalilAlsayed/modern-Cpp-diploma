fca793a72ae010f165a0e80e5b69cff373f2fcb1
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-02-16 13:17:37 +0200

    method channing

--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 #include "car.hpp"
 using namespace std;
 
+class Builder
+{
+public:
+	string str;
+
+	Builder(string str)
+	{
+		this->str = str;
+	}
+
+	/* method channing */
+	// void append(string copy){
+	// Builder append(const string &copy)
+	Builder &append(const string &copy)
+	{
+		str += copy;
+		return *this;
+	}
+};
 
 int main()
 {
+	Builder name("Ahmed ");
+
+	// name.append("Khalil ").append("Al "); without & Al won't append
+	name.append("Khalil ").append("Al ");
+
+	cout << name.str << endl;
 
 	return 0;
-} // here you avoid the <double free>
+}
