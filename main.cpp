323005510f23db74112a5c61d6778d8949145d10
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-04-05 09:48:40 +0200

    modern swap

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@
 #include "shared_ptr.cpp"
 #include <memory>
 
+void swapy(int &a, int &b)
+{
+	int temp = std::move(a);
+	a = std::move(b);
+	b = std::move(temp);
+}
+
 int main()
 {
+	int x = 5;
+	int y = 50;
+
+	swapy(x, y);
+
 	return 0;
 }
