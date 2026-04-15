3c728be524034b5f80a20a71b9e0d16bdf584aa2
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-04-16 00:50:59 +0200

    ...

--- database_factory.cpp
+++ database_factory.cpp
@@ -8,7 +8,7 @@ public:
 	virtual void connect() = 0;
 	virtual ~IConnection() = default;
 };
-class MySQLConnection : public IConnection
+class MySQLConnection final : public IConnection
 {
 public:
 	void connect() override
@@ -16,7 +16,7 @@ public:
 		std::cout << "MySQL Connection" << std::endl;
 	}
 };
-class PostgreSQLConnection : public IConnection
+class PostgreSQLConnection final : public IConnection
 {
 public:
 	void connect() override
@@ -32,7 +32,7 @@ public:
 	virtual void cmd() = 0;
 	virtual ~ICommand() = default;
 };
-class MySQLCommand : public ICommand
+class MySQLCommand final : public ICommand
 {
 public:
 	void cmd() override
@@ -40,12 +40,55 @@ public:
 		std::cout << "MySQL cmd" << std::endl;
 	}
 };
-class PostgreSQLCommand : public ICommand
+class PostgreSQLCommand final : public ICommand
 {
 public:
 	void cmd() override
 	{
 		std::cout << "PostgreSQL cmd" << std::endl;
+	}
+};
+
+class ITransaction
+{
+public:
+	virtual void begin() = 0;
+	virtual void commit() = 0;
+	virtual void rollback() = 0;
+	virtual ~ITransaction() = default;
+};
+
+class MySQLTransaction final : public ITransaction
+{
+public:
+	void begin() override
+	{
+		std::cout << "MySQLTransaction" << std::endl;
+	};
+	void commit() override
+	{
+		std::cout << "MySQLTransaction" << std::endl;
+	};
+	void rollback() override
+	{
+		std::cout << "MySQLTransaction" << std::endl;
+	}
+};
+
+class PostgreSQLTransaction final : public ITransaction
+{
+public:
+	void begin() override
+	{
+		std::cout << "PostgreSQLTransaction" << std::endl;
+	};
+	void commit() override
+	{
+		std::cout << "PostgreSQLTransaction" << std::endl;
+	};
+	void rollback() override
+	{
+		std::cout << "PostgreSQLTransaction" << std::endl;
 	}
 };
 
@@ -55,30 +98,41 @@ class IDatabaseConnectionFactory
 public:
 	virtual std::unique_ptr<IConnection> createConnection() = 0;
 	virtual std::unique_ptr<ICommand> createCommand() = 0;
+	virtual std::unique_ptr<ITransaction> createTransaction() = 0;
+
+	virtual ~IDatabaseConnectionFactory() = default;
 };
 
-class MySQLDatabaseConnectionFactory : public IDatabaseConnectionFactory
+class MySQLDatabaseConnectionFactory final : public IDatabaseConnectionFactory
 {
 public:
-	virtual std::unique_ptr<IConnection> createConnection() override
+	std::unique_ptr<IConnection> createConnection() override
 	{
 		return std::make_unique<MySQLConnection>();
 	}
-	virtual std::unique_ptr<ICommand> createCommand() override
+	std::unique_ptr<ICommand> createCommand() override
 	{
 		return std::make_unique<MySQLCommand>();
 	}
+	std::unique_ptr<ITransaction> createTransaction() override
+	{
+		return std::make_unique<MySQLTransaction>();
+	};
 };
 
-class PostgreSQLDatabaseConnectionFactory : public IDatabaseConnectionFactory
+class PostgreSQLDatabaseConnectionFactory final : public IDatabaseConnectionFactory
 {
 public:
-	virtual std::unique_ptr<IConnection> createConnection() override
+	std::unique_ptr<IConnection> createConnection() override
 	{
 		return std::make_unique<PostgreSQLConnection>();
 	}
-	virtual std::unique_ptr<ICommand> createCommand() override
+	std::unique_ptr<ICommand> createCommand() override
 	{
 		return std::make_unique<PostgreSQLCommand>();
 	}
+	std::unique_ptr<ITransaction> createTransaction() override
+	{
+		return std::make_unique<PostgreSQLTransaction>();
+	};
 };
