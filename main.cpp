38bb6d77ecda07ea77f944e31e310d09936f1c4c
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-05-05 15:07:11 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,49 +1,11 @@
-#include <iostream>
-#include <string.h>
-#include <utility>
+
 #include <memory>
-
-class UDT
-{
-private:
-	int data = 0;
-
-public:
-	UDT(/* args */);
-	~UDT();
-	static void print(std::weak_ptr<int> wptr)
-	{
-		// std::cout << wptr.use_count() << std::endl;
-		std::cout << wptr.expired() << std::endl;
-
-		auto sptr = wptr.lock();
-		std::cout << sptr.get() << std::endl;
-	}
-};
-
-UDT::UDT(/* args */)
-{
-	std::cout << "ctor" << std::endl;
-}
-
-UDT::~UDT()
-{
-	std::cout << "dtor" << std::endl;
-}
-
-class Free
-{
-public:
-	void operator()(int *ptr)
-	{
-		free(ptr);
-	}
-};
 
 int main()
 {
-	int *ptr = (int *)malloc(sizeof(int));
-	auto uptr = std::unique_ptr<int, Free>(ptr, Free{});
+	std::unique_ptr<int[]> p(new int[3]);
+
+	std::unique_ptr<int[]> pp = std::make_unique<int[]>(3);
 
 	return 0;
 }
