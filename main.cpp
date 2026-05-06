a7ac6843374d5656365eff1b290ca7137d1ca66c
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-05-06 15:02:28 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,25 +1,115 @@
+
 #include <iostream>
 #include <memory>
-#include <stdint.h>
-#include "singleton_parking.cpp"
-#include "widget_factory.cpp"
-#include "robo_factory.cpp"
-#include "builder.cpp"
+// A notification system that sends alerts via different channels.
+
+//// Step A – Write the BAD version first (to feel the pain)
+
+// enum ChannelType
+// {
+// 	email,
+// 	sms,
+// 	push,
+// slack
+// };
+
+// class Notifier
+// {
+// public:
+// 	void send(ChannelType type)
+// 	{
+// 		switch (type)
+// 		{
+// 		case ChannelType::email:
+// 			std::cout << "Sending email to user@example.com" << std::endl;
+// 			break;
+
+// 		case ChannelType::sms:
+// 			std::cout << "Sending SMS to +123456789" << std::endl;
+// 			break;
+
+// 		case ChannelType::push:
+// 			std::cout << "Sending push notification to device" << std::endl;
+// 			break;
+
+// 		default:
+// 			break;
+// 		}
+// 	}
+// };
+
+// int main()
+// {
+// 	Notifier notify;
+// 	notify.send(ChannelType::email);
+
+// 	return 0;
+// }
+
+//// Step B – Refactor to GOOD version following the principles
+
+class INotificationChannel
+{
+public:
+	virtual void send(const std::string &msg) = 0;
+	virtual ~INotificationChannel() = default;
+};
+
+class SlackChannel : public INotificationChannel
+{
+public:
+	void send(const std::string &msg) override
+	{
+		std::cout << "Sending to Slack" << std::endl;
+	}
+};
+
+class EmailChannel : public INotificationChannel
+{
+public:
+	void send(const std::string &msg) override
+	{
+		std::cout << "Sending email to user@example.com" << std::endl;
+	}
+};
+
+class SmsChannel : public INotificationChannel
+{
+public:
+	void send(const std::string &msg) override
+	{
+		std::cout << "Sending sms to" << std::endl;
+	}
+};
+class PushChannel : public INotificationChannel
+{
+public:
+	void send(const std::string &msg) override
+	{
+		std::cout << "Sending by pushing" << std::endl;
+	}
+};
+
+class Notifier
+{
+public:
+	void send(INotificationChannel &channel, const std::string &msg)
+	{
+		channel.send(msg);
+	}
+};
 
 int main()
 {
+	Notifier notify;
 
-	auto pizzaBuilder = PizzaBuilder();
-	auto director = PizzaDirector();
+	PushChannel pushChannel;
+	SmsChannel smsChannel;
+	EmailChannel emailChaneel;
 
-	Pizza pizza1 = director.make(pizzaBuilder, PizzaDirector::Pepperoni);
-	Pizza pizza2 = director.make(pizzaBuilder, PizzaDirector::Margherita);
-	Pizza pizza3 = director.make(pizzaBuilder, PizzaDirector::Pepperoni);
+	notify.send(pushChannel, "");
+	notify.send(emailChaneel, "");
+	notify.send(smsChannel, "");
 
-	pizza1.getPizza();
-	std::cout << "\n";
-	pizza2.getPizza();
-	std::cout << "\n";
-	pizza3.getPizza();
 	return 0;
 }
