b2248347e823782bb5803be234d45e7e1f5b974c
AhmedKhalilAlsayed <ahmedkhalilalsayed@gmail.com>
2026-06-03 14:59:43 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,30 +1,66 @@
 #include <iostream>
 #include <memory>
 
-class Base
+class IShape
 {
 public:
-	virtual void show()
+	virtual float getArea() const = 0;
+
+	virtual ~IShape() = default;
+};
+
+class Rectangle : public IShape
+{
+	float width_ = 0;
+	float height_ = 0;
+
+public:
+	Rectangle(float w, float h) : width_(w), height_(h) {}
+	float getArea() const override
 	{
-		std::cout << "Hello Base" << std::endl;
+		return width_ * height_;
 	}
 };
 
-class Derived : public Base
+class Square : public IShape
 {
-public:
-	void show() override
-	{
-		Base::show();
+	float side_ = 0;
 
-		// std::cout << "Hello Derived" << std::endl;
+public:
+	Square(float s) : side_(s) {}
+
+	float getArea() const override
+	{
+		return side_ * side_;
 	}
 };
+
+class Circle : public IShape
+{
+	float rad_ = 0;
+
+public:
+	Circle(float r) : rad_(r) {}
+	float getArea() const override
+	{
+		return 3.14 * rad_ * rad_;
+	}
+};
+
+/// @brief ////////////////////////
+void printArea(const IShape &shape)
+{
+	std::cout << "Area = " << shape.getArea() << std::endl;
+}
+////////////////////////////////////
 
 int main()
 {
-	Derived b{};
-	b.show();
+
+	Rectangle rect{123, 2};
+	Square sq{3};
+
+	printArea(sq);
 
 	return 0;
 }
